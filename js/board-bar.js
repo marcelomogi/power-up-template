bc818f4f2988c8f7b642570fbc6655399128fcd6
Marcelo Moraes <mmoraes@ciandt.com>
2017-05-21 10:25:41 -0300

    board

--- js/board-bar.js
+++ js/board-bar.js
@@ -5,10 +5,22 @@ var t = TrelloPowerUp.iframe();
 
 var myData = document.getElementById('myData');
 
+const levels = ['N1', 'N2', 'N3', 'N4'];
+
+board('all')
+  .then(function (success) {
+    console.log('Board:', success);
+
+  })
+  .catch(function (fail) {
+    console.log('Board Fail:', fail);
+  });
+
 t.render(function () {
   t.cards('all')
     .then(function (success) {
       console.log('Sucess:', success);
+
     })
     .catch(function (fail) {
       console.log('Fail:', fail);
